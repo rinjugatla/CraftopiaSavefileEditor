d516df2926d5b9d41cca859fe5a62a955470a87a
rinjugatla <rin.jugatla@gmail.com>
2021-10-30 17:16:18 +0900

    syntax highlight対応のテキストボックスを追加 OCS手動編集画面の機能を実装

--- CraftopiaSavefileEditor/Form1.Designer.cs
+++ CraftopiaSavefileEditor/Form1.Designer.cs
@@ -32,25 +32,25 @@ namespace CraftopiaSavefileEditor
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.OcsJsonConvert_TableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
             this.ConvertOcs2Json_Panel = new System.Windows.Forms.Panel();
+            this.ConvertOcs2Json_Label = new System.Windows.Forms.Label();
             this.ConvertJson2Ocs_Panel = new System.Windows.Forms.Panel();
+            this.ConvertJson2Ocs_Label = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.Manual_FileSave_Button = new System.Windows.Forms.Button();
+            this.Manual_FileBrowse_Button = new System.Windows.Forms.Button();
+            this.Manual_Open_Button = new System.Windows.Forms.Button();
+            this.Manual_Filepath_TextBox = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.Manual_Scintilla = new ScintillaNET.Scintilla();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.OcsJsonConvert_TableLayoutPanel.SuspendLayout();
-            this.tabPage2.SuspendLayout();
-            this.tableLayoutPanel2.SuspendLayout();
-            this.flowLayoutPanel1.SuspendLayout();
             this.ConvertOcs2Json_Panel.SuspendLayout();
             this.ConvertJson2Ocs_Panel.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -94,20 +94,61 @@ namespace CraftopiaSavefileEditor
             this.OcsJsonConvert_TableLayoutPanel.Size = new System.Drawing.Size(786, 418);
             this.OcsJsonConvert_TableLayoutPanel.TabIndex = 0;
             // 
-            // label2
+            // ConvertOcs2Json_Panel
             // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label2.Location = new System.Drawing.Point(76, 159);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(228, 40);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "JSONに変換";
+            this.ConvertOcs2Json_Panel.AllowDrop = true;
+            this.ConvertOcs2Json_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ConvertOcs2Json_Panel.Controls.Add(this.ConvertOcs2Json_Label);
+            this.ConvertOcs2Json_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ConvertOcs2Json_Panel.Location = new System.Drawing.Point(6, 6);
+            this.ConvertOcs2Json_Panel.Name = "ConvertOcs2Json_Panel";
+            this.ConvertOcs2Json_Panel.Size = new System.Drawing.Size(382, 406);
+            this.ConvertOcs2Json_Panel.TabIndex = 0;
+            this.ConvertOcs2Json_Panel.DragDrop += new System.Windows.Forms.DragEventHandler(this.ConvertOcs2Json_Panel_DragDrop);
+            this.ConvertOcs2Json_Panel.DragEnter += new System.Windows.Forms.DragEventHandler(this.Panel_DragEnter);
+            // 
+            // ConvertOcs2Json_Label
+            // 
+            this.ConvertOcs2Json_Label.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ConvertOcs2Json_Label.AutoSize = true;
+            this.ConvertOcs2Json_Label.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ConvertOcs2Json_Label.Location = new System.Drawing.Point(76, 159);
+            this.ConvertOcs2Json_Label.Name = "ConvertOcs2Json_Label";
+            this.ConvertOcs2Json_Label.Size = new System.Drawing.Size(228, 40);
+            this.ConvertOcs2Json_Label.TabIndex = 0;
+            this.ConvertOcs2Json_Label.Text = "JSONに変換";
+            // 
+            // ConvertJson2Ocs_Panel
+            // 
+            this.ConvertJson2Ocs_Panel.AllowDrop = true;
+            this.ConvertJson2Ocs_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ConvertJson2Ocs_Panel.Controls.Add(this.ConvertJson2Ocs_Label);
+            this.ConvertJson2Ocs_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ConvertJson2Ocs_Panel.Location = new System.Drawing.Point(397, 6);
+            this.ConvertJson2Ocs_Panel.Name = "ConvertJson2Ocs_Panel";
+            this.ConvertJson2Ocs_Panel.Size = new System.Drawing.Size(383, 406);
+            this.ConvertJson2Ocs_Panel.TabIndex = 0;
+            this.ConvertJson2Ocs_Panel.DragDrop += new System.Windows.Forms.DragEventHandler(this.ConvertJson2Ocs_Panel_DragDrop);
+            this.ConvertJson2Ocs_Panel.DragEnter += new System.Windows.Forms.DragEventHandler(this.Panel_DragEnter);
+            // 
+            // ConvertJson2Ocs_Label
+            // 
+            this.ConvertJson2Ocs_Label.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ConvertJson2Ocs_Label.AutoSize = true;
+            this.ConvertJson2Ocs_Label.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ConvertJson2Ocs_Label.Location = new System.Drawing.Point(95, 159);
+            this.ConvertJson2Ocs_Label.Name = "ConvertJson2Ocs_Label";
+            this.ConvertJson2Ocs_Label.Size = new System.Drawing.Size(207, 40);
+            this.ConvertJson2Ocs_Label.TabIndex = 0;
+            this.ConvertJson2Ocs_Label.Text = "OCSに変換";
             // 
             // tabPage2
             // 
-            this.tabPage2.Controls.Add(this.tableLayoutPanel2);
+            this.tabPage2.Controls.Add(this.panel1);
+            this.tabPage2.Controls.Add(this.Manual_FileSave_Button);
+            this.tabPage2.Controls.Add(this.Manual_FileBrowse_Button);
+            this.tabPage2.Controls.Add(this.Manual_Open_Button);
+            this.tabPage2.Controls.Add(this.Manual_Filepath_TextBox);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -115,6 +156,44 @@ namespace CraftopiaSavefileEditor
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "OCS手動編集";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Manual_FileSave_Button
+            // 
+            this.Manual_FileSave_Button.Location = new System.Drawing.Point(740, 6);
+            this.Manual_FileSave_Button.Name = "Manual_FileSave_Button";
+            this.Manual_FileSave_Button.Size = new System.Drawing.Size(44, 23);
+            this.Manual_FileSave_Button.TabIndex = 1;
+            this.Manual_FileSave_Button.Text = "保存";
+            this.Manual_FileSave_Button.UseVisualStyleBackColor = true;
+            this.Manual_FileSave_Button.Click += new System.EventHandler(this.Manual_FileSave_Button_Click);
+            // 
+            // Manual_FileBrowse_Button
+            // 
+            this.Manual_FileBrowse_Button.Location = new System.Drawing.Point(690, 6);
+            this.Manual_FileBrowse_Button.Name = "Manual_FileBrowse_Button";
+            this.Manual_FileBrowse_Button.Size = new System.Drawing.Size(44, 23);
+            this.Manual_FileBrowse_Button.TabIndex = 1;
+            this.Manual_FileBrowse_Button.Text = "参照";
+            this.Manual_FileBrowse_Button.UseVisualStyleBackColor = true;
+            this.Manual_FileBrowse_Button.Click += new System.EventHandler(this.Manual_FileBrowse_Button_Click);
+            // 
+            // Manual_Open_Button
+            // 
+            this.Manual_Open_Button.Location = new System.Drawing.Point(640, 6);
+            this.Manual_Open_Button.Name = "Manual_Open_Button";
+            this.Manual_Open_Button.Size = new System.Drawing.Size(44, 23);
+            this.Manual_Open_Button.TabIndex = 1;
+            this.Manual_Open_Button.Text = "開く";
+            this.Manual_Open_Button.UseVisualStyleBackColor = true;
+            this.Manual_Open_Button.Click += new System.EventHandler(this.Manual_Open_Button_Click);
+            // 
+            // Manual_Filepath_TextBox
+            // 
+            this.Manual_Filepath_TextBox.Location = new System.Drawing.Point(3, 8);
+            this.Manual_Filepath_TextBox.Margin = new System.Windows.Forms.Padding(5);
+            this.Manual_Filepath_TextBox.Name = "Manual_Filepath_TextBox";
+            this.Manual_Filepath_TextBox.Size = new System.Drawing.Size(629, 19);
+            this.Manual_Filepath_TextBox.TabIndex = 0;
             // 
             // tabPage3
             // 
@@ -125,93 +204,28 @@ namespace CraftopiaSavefileEditor
             this.tabPage3.Text = "MAP編集";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // Manual_Scintilla
             // 
-            this.textBox1.Location = new System.Drawing.Point(5, 5);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(602, 19);
-            this.textBox1.TabIndex = 0;
+            this.Manual_Scintilla.AutoCMaxHeight = 9;
+            this.Manual_Scintilla.BiDirectionality = ScintillaNET.BiDirectionalDisplayType.Disabled;
+            this.Manual_Scintilla.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Manual_Scintilla.Location = new System.Drawing.Point(0, 0);
+            this.Manual_Scintilla.Name = "Manual_Scintilla";
+            this.Manual_Scintilla.ScrollWidth = 49;
+            this.Manual_Scintilla.Size = new System.Drawing.Size(786, 386);
+            this.Manual_Scintilla.TabIndents = true;
+            this.Manual_Scintilla.TabIndex = 4;
+            this.Manual_Scintilla.UseRightToLeftReadingLayout = false;
+            this.Manual_Scintilla.WrapMode = ScintillaNET.WrapMode.None;
             // 
-            // button1
+            // panel1
             // 
-            this.button1.Location = new System.Drawing.Point(615, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "参照";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(696, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "保存";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // tableLayoutPanel2
-            // 
-            this.tableLayoutPanel2.ColumnCount = 1;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel1, 0, 0);
-            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
-            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 2;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(786, 418);
-            this.tableLayoutPanel2.TabIndex = 2;
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Controls.Add(this.textBox1);
-            this.flowLayoutPanel1.Controls.Add(this.button1);
-            this.flowLayoutPanel1.Controls.Add(this.button2);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(780, 34);
-            this.flowLayoutPanel1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(95, 159);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(207, 40);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "OCSに変換";
-            // 
-            // ConvertOcs2Json_Panel
-            // 
-            this.ConvertOcs2Json_Panel.AllowDrop = true;
-            this.ConvertOcs2Json_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.ConvertOcs2Json_Panel.Controls.Add(this.label2);
-            this.ConvertOcs2Json_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ConvertOcs2Json_Panel.Location = new System.Drawing.Point(6, 6);
-            this.ConvertOcs2Json_Panel.Name = "ConvertOcs2Json_Panel";
-            this.ConvertOcs2Json_Panel.Size = new System.Drawing.Size(382, 406);
-            this.ConvertOcs2Json_Panel.TabIndex = 0;
-            this.ConvertOcs2Json_Panel.DragDrop += new System.Windows.Forms.DragEventHandler(this.ConvertOcs2Json_Panel_DragDrop);
-            this.ConvertOcs2Json_Panel.DragEnter += new System.Windows.Forms.DragEventHandler(this.Panel_DragEnter);
-            // 
-            // ConvertJson2Ocs_Panel
-            // 
-            this.ConvertJson2Ocs_Panel.AllowDrop = true;
-            this.ConvertJson2Ocs_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.ConvertJson2Ocs_Panel.Controls.Add(this.label1);
-            this.ConvertJson2Ocs_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ConvertJson2Ocs_Panel.Location = new System.Drawing.Point(397, 6);
-            this.ConvertJson2Ocs_Panel.Name = "ConvertJson2Ocs_Panel";
-            this.ConvertJson2Ocs_Panel.Size = new System.Drawing.Size(383, 406);
-            this.ConvertJson2Ocs_Panel.TabIndex = 0;
-            this.ConvertJson2Ocs_Panel.DragDrop += new System.Windows.Forms.DragEventHandler(this.ConvertJson2Ocs_Panel_DragDrop);
-            this.ConvertJson2Ocs_Panel.DragEnter += new System.Windows.Forms.DragEventHandler(this.Panel_DragEnter);
+            this.panel1.Controls.Add(this.Manual_Scintilla);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(3, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(786, 386);
+            this.panel1.TabIndex = 5;
             // 
             // Form1
             // 
@@ -221,17 +235,17 @@ namespace CraftopiaSavefileEditor
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.OcsJsonConvert_TableLayoutPanel.ResumeLayout(false);
-            this.tabPage2.ResumeLayout(false);
-            this.tableLayoutPanel2.ResumeLayout(false);
-            this.flowLayoutPanel1.ResumeLayout(false);
-            this.flowLayoutPanel1.PerformLayout();
             this.ConvertOcs2Json_Panel.ResumeLayout(false);
             this.ConvertOcs2Json_Panel.PerformLayout();
             this.ConvertJson2Ocs_Panel.ResumeLayout(false);
             this.ConvertJson2Ocs_Panel.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -243,15 +257,16 @@ namespace CraftopiaSavefileEditor
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TableLayoutPanel OcsJsonConvert_TableLayoutPanel;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ConvertOcs2Json_Label;
+        private System.Windows.Forms.Button Manual_FileSave_Button;
+        private System.Windows.Forms.Button Manual_FileBrowse_Button;
+        private System.Windows.Forms.TextBox Manual_Filepath_TextBox;
+        private System.Windows.Forms.Label ConvertJson2Ocs_Label;
         private System.Windows.Forms.Panel ConvertOcs2Json_Panel;
         private System.Windows.Forms.Panel ConvertJson2Ocs_Panel;
+        private System.Windows.Forms.Button Manual_Open_Button;
+        private ScintillaNET.Scintilla Manual_Scintilla;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- CraftopiaSavefileEditor/Form1.cs
+++ CraftopiaSavefileEditor/Form1.cs
@@ -1,32 +1,135 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using CraftopiaSavefileEditor.Controller;
+using ScintillaNET;
 
 namespace CraftopiaSavefileEditor
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
-
-            InitForm();
         }
 
-        private void InitForm()
+        private void Form1_Load(object sender, EventArgs e)
         {
             this.MinimumSize = this.Size;
+
+            // https://github.com/robinrodricks/ScintillaNET.Demo/blob/b5f0e74bce15b8f18d0ffd5d5c7a8ba382162ff1/ScintillaNET.Demo/MainForm.cs
+            InitSyntaxColoring();
+            InitNumberMargin();
+            InitCodeFolding();
         }
 
+        #region Scintilla
+        private void InitHotkeys()
+        {
+
+            // register the hotkeys with the form
+            //HotKeyManager.AddHotKey(this, OpenSearch, Keys.F, true);
+            //HotKeyManager.AddHotKey(this, OpenFindDialog, Keys.F, true, false, true);
+            //HotKeyManager.AddHotKey(this, OpenReplaceDialog, Keys.R, true);
+            //HotKeyManager.AddHotKey(this, OpenReplaceDialog, Keys.H, true);
+            //HotKeyManager.AddHotKey(this, Uppercase, Keys.U, true);
+            //HotKeyManager.AddHotKey(this, Lowercase, Keys.L, true);
+            //HotKeyManager.AddHotKey(this, ZoomIn, Keys.Oemplus, true);
+            //HotKeyManager.AddHotKey(this, ZoomOut, Keys.OemMinus, true);
+            //HotKeyManager.AddHotKey(this, ZoomDefault, Keys.D0, true);
+            //HotKeyManager.AddHotKey(this, CloseSearch, Keys.Escape);
+
+            //// remove conflicting hotkeys from scintilla
+            //TextArea.ClearCmdKey(Keys.Control | Keys.F);
+            //TextArea.ClearCmdKey(Keys.Control | Keys.R);
+            //TextArea.ClearCmdKey(Keys.Control | Keys.H);
+            //TextArea.ClearCmdKey(Keys.Control | Keys.L);
+            //TextArea.ClearCmdKey(Keys.Control | Keys.U);
+
+        }
+
+        private Color RED = Color.FromArgb(255, 163, 21, 21);
+        private Color GREEN = Color.FromArgb(255, 0, 128, 0);
+        private void InitSyntaxColoring()
+        {
+            // Configure the default style
+            Manual_Scintilla.StyleResetDefault();
+            Manual_Scintilla.Styles[Style.Default].Font = "Consolas";
+            Manual_Scintilla.Styles[Style.Default].Size = 10;
+            Manual_Scintilla.StyleClearAll();
+
+            // https://tewarid.github.io/2017/02/07/json-syntax-highlighting-in-scintillanet.html
+            Manual_Scintilla.Styles[Style.Json.Default].ForeColor = Color.Silver;
+            Manual_Scintilla.Styles[Style.Json.BlockComment].ForeColor = GREEN;
+            Manual_Scintilla.Styles[Style.Json.LineComment].ForeColor = GREEN;
+            Manual_Scintilla.Styles[Style.Json.Number].ForeColor = Color.Olive;
+            Manual_Scintilla.Styles[Style.Json.PropertyName].ForeColor = Color.Blue;
+            Manual_Scintilla.Styles[Style.Json.String].ForeColor = RED;
+            Manual_Scintilla.Styles[Style.Json.StringEol].BackColor = Color.Pink;
+            Manual_Scintilla.Styles[Style.Json.Operator].ForeColor = Color.Purple;
+            Manual_Scintilla.Lexer = Lexer.Json;
+        }
+
+        private const int NUMBER_MARGIN = 1;
+        private void InitNumberMargin()
+        {
+            var nums = Manual_Scintilla.Margins[NUMBER_MARGIN];
+            nums.Width = 30;
+            nums.Type = MarginType.Number;
+            nums.Sensitive = true;
+            nums.Mask = 0;
+        }
+
+        private Color BACK_COLOR = Color.FromArgb(255, 42, 33, 28);
+        private Color FORE_COLOR = Color.FromArgb(255, 183, 183, 183);
+        private const int FOLDING_MARGIN = 3;
+        private const bool CODEFOLDING_CIRCULAR = true;
+        private void InitCodeFolding()
+        {
+            // Enable code folding
+            Manual_Scintilla.SetProperty("fold", "1");
+            Manual_Scintilla.SetProperty("fold.compact", "1");
+
+            // Configure a margin to display folding symbols
+            Manual_Scintilla.Margins[FOLDING_MARGIN].Type = MarginType.Symbol;
+            Manual_Scintilla.Margins[FOLDING_MARGIN].Mask = Marker.MaskFolders;
+            Manual_Scintilla.Margins[FOLDING_MARGIN].Sensitive = true;
+            Manual_Scintilla.Margins[FOLDING_MARGIN].Width = 20;
+
+            // Set colors for all folding markers
+            for (int i = 25; i <= 31; i++)
+            {
+                Manual_Scintilla.Markers[i].SetForeColor(BACK_COLOR); // styles for [+] and [-]
+                Manual_Scintilla.Markers[i].SetBackColor(FORE_COLOR); // styles for [+] and [-]
+            }
+
+            // Configure folding markers with respective symbols
+            Manual_Scintilla.Markers[Marker.Folder].Symbol = CODEFOLDING_CIRCULAR ? MarkerSymbol.CirclePlus : MarkerSymbol.BoxPlus;
+            Manual_Scintilla.Markers[Marker.FolderOpen].Symbol = CODEFOLDING_CIRCULAR ? MarkerSymbol.CircleMinus : MarkerSymbol.BoxMinus;
+            Manual_Scintilla.Markers[Marker.FolderEnd].Symbol = CODEFOLDING_CIRCULAR ? MarkerSymbol.CirclePlusConnected : MarkerSymbol.BoxPlusConnected;
+            Manual_Scintilla.Markers[Marker.FolderMidTail].Symbol = MarkerSymbol.TCorner;
+            Manual_Scintilla.Markers[Marker.FolderOpenMid].Symbol = CODEFOLDING_CIRCULAR ? MarkerSymbol.CircleMinusConnected : MarkerSymbol.BoxMinusConnected;
+            Manual_Scintilla.Markers[Marker.FolderSub].Symbol = MarkerSymbol.VLine;
+            Manual_Scintilla.Markers[Marker.FolderTail].Symbol = MarkerSymbol.LCorner;
+
+            // Enable automatic folding
+            Manual_Scintilla.AutomaticFold = (AutomaticFold.Show | AutomaticFold.Click | AutomaticFold.Change);
+
+        }
+        #endregion
+
+        #region ScintillaNET-FindReplaceDialog
+        //private FindReplace MyFindReplace;
+
+
+        #endregion
+
+        #region OCS-JSON変換
         private void Panel_DragEnter(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
@@ -52,5 +155,61 @@ namespace CraftopiaSavefileEditor
             foreach (var json in jsons)
                 OcsController.ConvertJson2Ocs(json);
         }
+        #endregion
+
+        #region OCS手動編集
+        private void Manual_Open_Button_Click(object sender, EventArgs e)
+        {
+            string path = Manual_Filepath_TextBox.Text;
+            if (path == "" || !File.Exists(path))
+            {
+                MessageBox.Show("ファイルが存在しません。", "エラー", MessageBoxButtons.OK ,MessageBoxIcon.Information);
+                return;
+            }
+            if(Path.GetExtension(path) != ".ocs")
+            {
+                MessageBox.Show("ocs以外のファイルが選択されています。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            Manual_Scintilla.Text = OcsController.LoadOcs(path);
+        }
+
+        private void Manual_FileBrowse_Button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog() {
+                Filter = "OCSファイル(*.ocs)|*.ocs",
+                Title = "OCSファイルを選択",
+                RestoreDirectory = true
+            };
+
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                string path = ofd.FileName;
+                Manual_Filepath_TextBox.Text = path;
+                Manual_Scintilla.Text = OcsController.LoadOcs(path);
+            }
+        }
+
+        private void Manual_FileSave_Button_Click(object sender, EventArgs e)
+        {
+            string path = Manual_Filepath_TextBox.Text;
+            if (path == "")
+            {
+                MessageBox.Show("保存先が不明です。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            try
+            {
+                //OcsController.SaveOcs(path, Manual_Scintilla.Text);
+                MessageBox.Show("OCSファイルを保存しました。", "情報", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"OCSファイルの保存に失敗しました。\r\nエラー詳細: {ex.Message}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        #endregion
     }
 }
